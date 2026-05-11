c03255c4c64d81535d2bb95c4b3fab81813af1b2
Alvaro-76 <alvaro.976@gmail.com>
2026-05-11 23:56:49 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,26 @@
-import { NavLink } from "react-router-dom";
+// components/NavBar.jsx
+import { NavLink } from "react-router-dom"
 
-
-function Navbar(){
-    return (
-        <nav className="navbar">
-            <NavLink className="navbar__link" to="/" end>
-                Inicio
-            </NavLink>
-
-            <NavLink className="navbar__link" to="/education">
-                Estudios
-            </NavLink>
-
-            <NavLink className="navbar__link" to="/experience">
-                Experiencia
-            </NavLink>
-
-            <NavLink className="navbar__link" to="/skills">
-                Habilidades
-            </NavLink>
-
-            <NavLink className="navbar__link" to="/contact">
-                Contacto
-            </NavLink>
-      
-        </nav>
-    )       
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <NavLink className="navbar__link" to="/" end>
+        Inicio
+      </NavLink>
+      <NavLink className="navbar__link" to="/education">
+        Estudios
+      </NavLink>
+      <NavLink className="navbar__link" to="/experience">
+        Experiencia
+      </NavLink>
+      <NavLink className="navbar__link" to="/skills">
+        Habilidades
+      </NavLink>
+      <NavLink className="navbar__link" to="/contact">
+        Contacto
+      </NavLink>
+    </nav>
+  )
 }
 
-export default Navbar
+export default NavBar
