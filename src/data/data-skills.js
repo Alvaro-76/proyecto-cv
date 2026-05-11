2bac7bf608b0b54b89b02a3ee3e11c5b014a9141
Alvaro-76 <alvaro.976@gmail.com>
2026-05-11 19:43:51 +0200

    Update data-skills.js

--- src/data/data-skills.js
+++ src/data/data-skills.js
@@ -1,5 +1,52 @@
 export const skills = [
     {
-        
+        skillsid: 1,
+        name: "JavaScript",        
+        category: "programming"
+    },
+    {
+        skillsid: 2,
+        name: "HTML",
+        category: "programming"
+    },
+    {
+        skillsid: 3,
+        name: "CSS",
+        category: "programming" 
+    },
+    {
+        skillsid: 4,
+        name: "React",
+        category: "programming"
+    },
+    {
+        skillsid: 5,    
+        name: "Angular.js",
+        category: "programming"
+    },  
+    {
+        skillsid: 6,
+        name: "Java",
+        category: "programming"
+    },
+    {
+        skillsid: 7,
+        name: "Adobe Photoshop",
+        category: "design"
+    },
+    {
+        skillsid: 8,
+        name: "Blender 3D",
+        category: "design"
+    },
+    {
+        skillsid: 9,
+        name: "PHP",
+        category: "programming"
+    },
+    {
+        skillsid: 10,
+        name: "MySQL",
+        category: "programming"
     }
 ]
