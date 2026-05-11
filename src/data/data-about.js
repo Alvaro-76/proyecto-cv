3cbd0c68a74295728394ce2c1cf396061c7b5f2a
Alvaro-76 <alvaro.976@gmail.com>
2026-05-11 19:44:07 +0200

    Update data-about.js

--- src/data/data-about.js
+++ src/data/data-about.js
@@ -1,5 +1,11 @@
 export const about = [
     {
-        
+        name:"Álvaro Blanco Alarcón",
+        description: "Desarrollador autodidacta con experiencia en programación en C++ y C#, orientado principalmente al desarrollo de software y videojuegos. He desarrollado proyectos personales utilizando C++ y la librería Allegro, así como con Unity y C# en el ámbito del desarrollo de videojuegos. También tengo experiencia en desarrollo web con PHP y bases de datos MySQL. Me defino como una persona autodidacta, con capacidad de aprendizaje continuo y adaptación a diferentes tecnologías y entornos de desarrollo.",
+        date: 1976,
+        address: "Marbella, Málaga, España",
+        itchio: "https://kodaygames.itch.io/",
+        github: "https://github.com/Alvaro-76",
+        blog: "https://devcpp-allegro.blogspot.com/"    
     }
 ]
