f48d4c1460e847e6a010633fcbcedee98d76e226
Alvaro-76 <alvaro.976@gmail.com>
2026-05-11 19:44:04 +0200

    Update data-educations.js

--- src/data/data-educations.js
+++ src/data/data-educations.js
@@ -1,5 +1,16 @@
 export const education = [
     {
-        
+        id: 1,
+        title: "FP1 - Auxiliar Administrativo",
+        institution: "IES Guadalpín",
+        dateStar: "15/09/1990",
+        dateEnd: "30/06/1992"        
+    },
+    {
+        id: 2,
+        title: "FP2 - Informática de Gestión",
+        institution: "IES Guadalpín",
+        dateStar: "15/09/1992",
+        dateEnd: "30/06/1994"
     }
 ]
