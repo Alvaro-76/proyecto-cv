d97282a8c5434e69450adf675c723386a90dd6d0
Alvaro-76 <alvaro.976@gmail.com>
2026-05-11 23:56:21 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,39 @@
-import about from "./data/data-about"
+// pages/Home.jsx
+import { about } from "../data/data-about"
 
-function Home(){
-    return(
-        <div>
-            <h1>Home</h1>
-            <p>Esta es mi página de inicio.</p>
+function Home() {
+  return (
+    <div>
+      <section>
+        <h1>{about.name}</h1>
+        <p>{about.email} | Fecha Nacimiento: {about.date} | {about.address}</p>
+      </section>
 
-
-            <p>información a distribuir entre las varias páginas</p>
-
-        </div>
-    )
+      <section>
+        <h2>Sobre mi</h2>
+        <p>{about.description}</p>
+      </section>
+      
+      <section className="links-section">
+        <h2>Enlaces</h2>
+        <p>
+          <a href={about.itchio} target="_blank" rel="noopener noreferrer">
+            {about.itchio}
+          </a>
+        </p>
+        <p>
+          <a href={about.github} target="_blank" rel="noopener noreferrer">
+            {about.github}
+          </a>
+        </p>
+        <p>
+          <a href={about.blog} target="_blank" rel="noopener noreferrer">
+            {about.blog}
+          </a>
+        </p>
+      </section>
+    </div>
+  )
 }
 
 export default Home
