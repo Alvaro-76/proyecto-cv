3e592ef07a6c92d4e76d35b2eb6ea8181eaf4ccf
Alvaro-76 <alvaro.976@gmail.com>
2026-05-11 23:56:54 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 function Footer(){
     return(
         <footer>
-            <p>Pie de pagina</p>
+            <p>Proyecto creado con React. Para el Curso FullStack JavaScript con React y Node</p>
+            <p>© {new Date().getFullYear()} - Curriculum Vitae</p>
         </footer>
     )
 }
