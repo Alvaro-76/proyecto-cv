7c11eeacb3375bc60daedf10be9aa7f618568751
Alvaro-76 <alvaro.976@gmail.com>
2026-05-11 23:56:28 +0200

    Update MainLayout.jsx

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,14 +2,12 @@ import { Outlet } from 'react-router-dom'
 
 import Header from "../Components/Header"
 import Footer from "../Components/Footer"
-import NavBar from "../Components/NavBar"
 
 function MainLayout() {
     return (
         <main>
             <Header />
-            <NavBar />
-
+            
             <Outlet />  
 
             <Footer />
