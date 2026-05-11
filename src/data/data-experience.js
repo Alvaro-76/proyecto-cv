5da211c055dad2c62473657c9f91a40b77982810
Alvaro-76 <alvaro.976@gmail.com>
2026-05-11 19:43:54 +0200

    Update data-experience.js

--- src/data/data-experience.js
+++ src/data/data-experience.js
@@ -1,5 +1,26 @@
 export const experience = [
     {
-        
+        expid: 1,
+        title: "Autonomo, Venta material informático.",
+        company: "Autonomo",
+        dateStar: "01/02/2001",
+        dateEnd: "30/12/2008",
+        description: "Venta y Montaje de material informático a empresas y particulares."
+    },
+    {
+        expid: 2,
+        title: "Cajero, reponedor.",
+        company: "MiniMarket Elviria.",
+        dateStar: "01/07/2009",
+        dateEnd: "31/10/2012",
+        description: "Cajero, reponedor y mantenimiento informático en supermercado."        
+    },
+    {
+        expid: 3,
+        title: "Pinche de cocina.",
+        company: "Blanco Bistro S.L.",
+        dateStar: "24/10/2019",
+        dateEnd: "24/01/2020",
+        description: "Pinche de cocina en restaurante."
     }
 ]
