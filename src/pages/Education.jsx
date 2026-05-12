1707e4f1698c6e0efb066c31a3688d7f154d71fc
Alvaro-76 <alvaro.976@gmail.com>
2026-05-12 09:31:33 +0200

    Update Education.jsx

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,11 +1,25 @@
+import {education} from '../data/data-educations.js'
 function Education(){
 
     return(
         <>
+        <div>
+            <section>
+                <div>
+                    <h1>Formación Académica</h1>
+                </div>
+            </section>
+            
             <div>
-                <h1>Estudios</h1>
-                <p>Estos son mis estudios.</p>
-            </div>            
+                {education.map((education) => (
+                    <section className="resaltado">
+                        <h2>{education.title}</h2>
+                        <p>{education.institution} | {education.dateStar} - {education.dateEnd}</p>
+                    </section>
+                ))}
+            </div>
+
+        </div>                
         </>
     )
 }
