0fb1084500fc3bc933b5b551fe4433a3b19b7ac2
Alvaro-76 <alvaro.976@gmail.com>
2026-05-12 09:31:26 +0200

    Update Skills.jsx

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,42 @@
+import { skills } from "../data/data-skills";
+
 function Skills() {
+
+    const groupedSkills = skills.reduce((acc, skill) => {
+    const category = skill.category;
+
+        if (!acc[category]) {
+            acc[category] = [];
+        }
+
+        acc[category].push(skill);
+
+        return acc;
+    }, {});
+
+
     return (
         <div>
-            <h1>Habilidades</h1>
-            <p>Estas son mis habilidades.</p>
+            <section>
+                <h1>Habilidades técnicas</h1>
+                <p>He trabajado y desarrollado proyectos usando estas tecnologías</p>
+            </section>
+            
+            <div>
+                {Object.entries(groupedSkills).map(([category, skills]) => (
+                    <section key={category}>
+                        <h2>{category}</h2>
+
+                        <div className="skills">
+                            {skills.map((skill) => (
+                                <span key={skill.id} className="skill-tag">
+                                    {skill.name}
+                                </span>
+                            ))}
+                        </div>
+                    </section>
+                ))}
+            </div>
         </div>
     );
 }   
