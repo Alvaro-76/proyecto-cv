20f8202944601a53fc5e0de5234112d3bae45938
Alvaro-76 <alvaro.976@gmail.com>
2026-05-12 11:48:33 +0200

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,37 @@
-function Contact(){
-    return(
+function Contact() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Mensaje simulando envio");
+    };
+
+    return (
         <div className="page">
             <h1>Contacto</h1>
-            <p>Puedes escribime para resolver cualquier duda.</p>
+            <p>Puedes escribirme para resolver cualquier duda.</p>
 
             <div className="contact-form-wrapper">
-                <form action="#" method="post" encType="text/plain" className="contact-form" style={{
-                            "--rotate": `${Math.random() * 6 - 3}deg`
-                        }}
-                        >
-                    <label for="name">Nombre:</label>
-                    <input type="text" id="name" name="name" required placeholder="Escribe tu nombre"  />
+                <form
+                    onSubmit={handleSubmit}
+                    className="contact-form"
+                    style={{
+                        "--rotate": `${Math.random() * 6 - 3}deg`
+                    }}
+                >
+                    <label htmlFor="name">Nombre:</label>
+                    <input type="text" id="name" name="name" required placeholder="Escribe tu nombre" />
 
-                    <label for="email">Email:</label>
+                    <label htmlFor="email">Email:</label>
                     <input type="email" id="email" name="email" required placeholder="Escribe tu email" />
 
-                    <label for="message">Mensaje:</label>   
+                    <label htmlFor="message">Mensaje:</label>
                     <textarea id="message" name="message" required placeholder="Escribe tu mensaje"></textarea>
 
                     <input type="submit" value="Enviar" />
-                </form> 
+                </form>
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
