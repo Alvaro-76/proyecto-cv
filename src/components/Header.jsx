74007a5926dc788801ebdc13d2d69b02cb476a80
Alvaro-76 <alvaro.976@gmail.com>
2026-05-13 18:14:56 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import NavBar from "./NavBar"
 function Header() {
   return (
     <header>
-      <h1>Currículum Vitae</h1>
+      <h1>Curriculum Vitae</h1>
       <NavBar />     
     </header>
   )
