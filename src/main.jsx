99d6395d6f6cec51cb0d6a8137eab4f00da68a9a
alvaro <alvaro.976@gmail.com>
2026-06-10 12:19:11 +0200

    added: basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// Detecta si está en GitHub Pages o Vercel
+const basename = window.location.hostname.includes('github.io') ? '/proyecto-cv' : '/'
+console.log(window.location.hostname)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/proyecto-cv">
